af1bd7e2b713bea0914a2745b535b073e3c332a6
ivanderlich@gmail.com <ivanderlich@gmail.com>
2019-07-04 21:22:28 -0300

    Minor change

--- spec/enumerables_spec.rb
+++ spec/enumerables_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Enumerable do
     describe " #my_each" do
         it "test1" do
             expect([1,4,3,2].my_each{
-                |x| return x
+                |x| x
             }).to eql([1,4,3,2])
         end
     end
